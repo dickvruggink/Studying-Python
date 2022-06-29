3c8e6b2668fd272cd5b981ae966f76f0d0160b14
Dick Vruggink <dickvruggink@gmail.com>
2022-06-29 17:10:30 +0200

    Justified splitting first- and last_name

--- strings/main.py
+++ strings/main.py
@@ -15,19 +15,21 @@ print(scorers)
 report = f"{player1} scored in the {goal_0}nd minute\n{player2} scored in the {goal_1}th minute"
 print(report)
 
-player = "Frankie Rijkaard"
-first_name = player[0:7]
-last_name = player[8:]
-last_name_len = player.find("Rijkaard")
+player = "Frank Rijkaard"
+a = player.find(" "[:])
+
+first_name = (player[:a])
+last_name = (player[a+1:])
+
+
+last_name_len = len(last_name)
 print(last_name_len)
 name_short = (player[0:1] + ". " + last_name )
 print(name_short)
 
 chant = ((first_name + "! ") * len(first_name))
 txt = chant
-chant = txt.rstrip(" ")
+chant = txt.rstrip(" >")
 print(chant)
 
 good_chant = chant != ' '
-
-
