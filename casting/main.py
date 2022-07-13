5412fb81b35dcbdcf2e798bb0380264075f595c1
Dick Vruggink <dickvruggink@gmail.com>
2022-07-13 14:02:25 +0200

    Added exercise casting

--- casting/main.py
+++ casting/main.py
@@ -7,6 +7,9 @@ __human_name__ = 'casting'
 #Part 1
 
 
+from decimal import ROUND_UP
+
+
 leek_price = str(2)
 print('Leek is ' + leek_price + ' euro per kilo.')
 
@@ -17,3 +20,9 @@ order_amount = int(order[order.find(' '):])
 leek_sum_total = (order_amount * int(leek_price))
 print(leek_sum_total)
 
+#Part 3
+
+broccoli_kilo_price = 2.34
+order_amount_broccoli = 1.6
+total_price_broccoli = (broccoli_kilo_price * order_amount_broccoli)
+print('1.6kg broccoli costs ' + str(round(total_price_broccoli, 2)) + 'e')
