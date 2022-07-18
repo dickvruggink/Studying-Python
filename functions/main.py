7d2737a2edb989c427c8ab128ab861639b647add
Dick Vruggink <dickvruggink@gmail.com>
2022-07-18 14:42:51 +0200

    Added functions and statements

---
+++ functions/main.py
@@ -0,0 +1,43 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+
+# from tkinter import N
+
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# name_student = greet('Bob')
+# print(name_student)
+
+# def add(a, b ,c):
+#     return a + b + c
+
+# sum = add(3, 7 ,5)
+# print(sum)
+
+# def positive(x):
+#     return x > 0
+
+# number = positive(3)
+# print(number)
+
+# def negative(x):
+#     return x < 0
+
+# number = negative(0)
+# print(number)
+
+
+def main():
+    print(greet('Bob'))
+    
+    
+def greet(name):
+        return f'Hello,\n{name}!'
+
+if __name__ == '__main__':           
+    main()
